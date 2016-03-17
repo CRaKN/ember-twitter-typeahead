efea9efea8232b0524a60af4d069611919e6db57
Anthony Isaacs <isaacs.anthony@gmail.com>
2016-03-17 18:31:01 -0400

    Further updating dependencies and fixing index.html

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import testLoader from 'ember-cli/test-loader';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
